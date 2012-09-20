3ca53a0ffed8d6afe687c959a5a2e76bab922e47
Hawra Abdulla <hawraabdulla@Hawras-MacBook-Air.local>
2012-09-20 16:49:56 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuT::Application.config.secret_token = '83099526c6fe12ae93e526241f90f7e04d7877001cb5aa10e7985aafcf59ad8bd42ea792660776697647ac669b422faa04bd0e4e328fadd47c8bfde7f35b6796'
